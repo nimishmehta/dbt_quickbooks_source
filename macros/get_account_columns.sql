4dc08bf2ac9a7937cb0d97725e99a4af2c558b6a
Nimish Mehta <nimish@ip-192-168-29-251.us-west-1.compute.internal>
2021-11-18 12:17:39 +0530

    accounts added

--- macros/get_account_columns.sql
+++ macros/get_account_columns.sql
@@ -1,25 +1,25 @@
 {% macro get_account_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
-    {"name": "account_number", "datatype": dbt_utils.type_string()},
-    {"name": "account_sub_type", "datatype": dbt_utils.type_string()},
-    {"name": "account_type", "datatype": dbt_utils.type_string()},
-    {"name": "active", "datatype": "boolean"},
-    {"name": "balance", "datatype": dbt_utils.type_float()},
-    {"name": "balance_with_sub_accounts", "datatype": dbt_utils.type_float()},
-    {"name": "classification", "datatype": dbt_utils.type_string()},
-    {"name": "created_at", "datatype": dbt_utils.type_timestamp()},
-    {"name": "currency_id", "datatype": dbt_utils.type_int()},
-    {"name": "description", "datatype": dbt_utils.type_string()},
-    {"name": "fully_qualified_name", "datatype": dbt_utils.type_string()},
-    {"name": "id", "datatype": dbt_utils.type_int()},
-    {"name": "name", "datatype": dbt_utils.type_string()},
-    {"name": "parent_account_id", "datatype": dbt_utils.type_int()},
-    {"name": "sub_account", "datatype": "boolean"},
-    {"name": "sync_token", "datatype": dbt_utils.type_string()},
-    {"name": "tax_code_id", "datatype": dbt_utils.type_int()},
-    {"name": "updated_at", "datatype": dbt_utils.type_timestamp()}
+    {"name": "_fivetran_synced", "alias": "_fivetran_synced", "quote": "true", "datatype": dbt_utils.type_timestamp()},
+    {"name": "acctnum", "alias": "account_number", "quote": "true", "datatype": dbt_utils.type_string()},
+    {"name": "accountsubtype", "alias": "account_sub_type", "quote": "true", "datatype": dbt_utils.type_string()},
+    {"name": "accounttype", "alias": "account_type", "quote": "true", "datatype": dbt_utils.type_string()},
+    {"name": "active", "alias": "active", "quote": "true", "datatype": "boolean"},
+    {"name": "currentbalance", "alias": "balance", "quote": "true", "datatype": dbt_utils.type_float()},
+    {"name": "currentbalancewithsubaccounts", "alias": "balance_with_sub_accounts", "quote": "true", "datatype": dbt_utils.type_float()},
+    {"name": "classification", "alias": "classification", "quote": "true", "datatype": dbt_utils.type_string()},
+    {"name": "created_at", "alias": "created_at", "quote": "true", "datatype": dbt_utils.type_timestamp()},
+    {"name": "currency_id", "alias": "currency_id", "quote": "true", "datatype": dbt_utils.type_int()},
+    {"name": "description", "alias": "description", "quote": "true", "datatype": dbt_utils.type_string()},
+    {"name": "fullyqualifiedname", "alias": "fully_qualified_name", "quote": "true", "datatype": dbt_utils.type_string()},
+    {"name": "Id", "alias": "id", "quote": "true", "datatype": dbt_utils.type_int()},
+    {"name": "Name", "alias": "name", "quote": "true", "datatype": dbt_utils.type_string()},
+    {"name": "parent_account_id", "alias": "parent_account_id", "quote": "true", "datatype": dbt_utils.type_int()},
+    {"name": "subaccount", "alias": "sub_account", "quote": "true", "datatype": "boolean"},
+    {"name": "sync_token", "alias": "sync_token", "quote": "true", "datatype": dbt_utils.type_string()},
+    {"name": "tax_code_id", "alias": "tax_code_id", "quote": "true", "datatype": dbt_utils.type_int()},
+    {"name": "updated_at", "alias": "updated_at", "quote": "true", "datatype": dbt_utils.type_timestamp()}
 ] %}
 
 {{ return(columns) }}
--- models/tmp/stg_quickbooks__account_tmp.sql
+++ models/tmp/stg_quickbooks__account_tmp.sql
@@ -1,2 +1,10 @@
-select *
+select
+    {{ var('accounts') }}.*,
+    {{ var('accounts_currencyref') }}.value as currency_id,
+    {{ var('accounts_metadata') }}.createtime as created_at,
+    {{ var('accounts_metadata') }}.lastupdatedtime as updated_at,
+    {{ var('accounts_parentref') }}.value as parent_account_id
 from {{ var('accounts') }}
+left join {{ var('accounts_currencyref') }} using (_airbyte_accounts_hashid)
+left join {{ var('accounts_metadata') }} using (_airbyte_accounts_hashid)
+left join {{ var('accounts_parentref') }} using (_airbyte_accounts_hashid)
