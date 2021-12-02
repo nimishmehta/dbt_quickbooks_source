65e1879bb4df528d1b78d02128af91bd6f2caf18
vigneshnallamad <vignesh.nm@medlife.com>
2021-12-03 01:45:59 +0530

    Fixed Invoice

--- macros/get_invoice_line_bundle_columns.sql
+++ macros/get_invoice_line_bundle_columns.sql
@@ -1,26 +1,25 @@
 {% macro get_invoice_line_bundle_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
-    {"name": "account_id", "datatype": dbt_utils.type_int()},
-    {"name": "amount", "datatype": dbt_utils.type_float()},
-    {"name": "class_id", "datatype": dbt_utils.type_int()},
-    {"name": "description", "datatype": dbt_utils.type_string()},
-    {"name": "discount_amount", "datatype": dbt_utils.type_float()},
-    {"name": "discount_rate", "datatype": dbt_utils.type_float()},
-    {"name": "index", "datatype": dbt_utils.type_int()},
-    {"name": "invoice_id", "datatype": dbt_utils.type_int()},
-    {"name": "invoice_line_index", "datatype": dbt_utils.type_int()},
-    {"name": "item_id", "datatype": dbt_utils.type_int()},
-    {"name": "line_num", "datatype": dbt_utils.type_int()},
-    {"name": "quantity", "datatype": dbt_utils.type_float()},
-    {"name": "sales_item_account_id", "datatype": dbt_utils.type_int()},
-    {"name": "sales_item_item_id", "datatype": dbt_utils.type_int()},
-    {"name": "sales_item_quantity", "datatype": dbt_utils.type_float()},
-    {"name": "sales_item_tax_code_id", "datatype": dbt_utils.type_int()},
-    {"name": "service_date", "datatype": dbt_utils.type_timestamp()},
-    {"name": "tax_code_id", "datatype": dbt_utils.type_int()},
-    {"name": "unit_price", "datatype": dbt_utils.type_float()}
+    {"name": "account_id", "alias": "account_id", "datatype": dbt_utils.type_int(), "quote": true},
+    {"name": "amount", "alias": "amount", "datatype": dbt_utils.type_float(), "quote": true},
+    {"name": "class_id", "alias": "class_id", "datatype": dbt_utils.type_int(), "quote": true},
+    {"name": "description", "alias": "description", "datatype": dbt_utils.type_string(), "quote": true},
+    {"name": "discount_amount", "alias": "discount_amount", "datatype": dbt_utils.type_float(), "quote": true},
+    {"name": "discount_rate", "alias": "discount_rate", "datatype": dbt_utils.type_float(), "quote": true},
+    {"name": "index", "alias": "index", "datatype": dbt_utils.type_int(), "quote": true},
+    {"name": "invoice_id", "alias": "invoice_id", "datatype": dbt_utils.type_int(), "quote": true},
+    {"name": "invoice_line_index", "alias": "invoice_line_index", "datatype": dbt_utils.type_int(), "quote": true},
+    {"name": "item_id", "alias": "item_id", "datatype": dbt_utils.type_int(), "quote": true},
+    {"name": "line_num", "alias": "line_num", "datatype": dbt_utils.type_int(), "quote": true},
+    {"name": "quantity", "alias": "quantity", "datatype": dbt_utils.type_float(), "quote": true},
+    {"name": "sales_item_account_id", "alias": "sales_item_account_id", "datatype": dbt_utils.type_int(), "quote": true},
+    {"name": "sales_item_item_id", "alias": "sales_item_item_id", "datatype": dbt_utils.type_int(), "quote": true},
+    {"name": "sales_item_quantity", "alias": "sales_item_quantity", "datatype": dbt_utils.type_float(), "quote": true},
+    {"name": "sales_item_tax_code_id", "alias": "sales_item_tax_code_id", "datatype": dbt_utils.type_int(), "quote": true},
+    {"name": "service_date", "alias": "service_date", "datatype": dbt_utils.type_timestamp(), "quote": true},
+    {"name": "tax_code_id", "alias": "tax_code_id", "datatype": dbt_utils.type_int(), "quote": true},
+    {"name": "unit_price", "alias": "unit_price", "datatype": dbt_utils.type_float(), "quote": true}
 ] %}
 
 {{ return(columns) }}
--- models/tmp/stg_quickbooks__invoice_line_tmp.sql
+++ models/tmp/stg_quickbooks__invoice_line_tmp.sql
@@ -3,15 +3,15 @@
 
 select 
 {{ var('invoices_line') }}.*,
-{{ ('invoices_line_discou__il_discountaccountref') }}.value as discount_account_id,
-
+{{ var('invoices_line_discou__il_discountaccountref') }}.value as discount_account_id,
+{{ var('invoices_line_discountlinedetail_classref') }}.value as discount_class_id
 from {{ var('invoices_line') }}
 left join {{ var('invoices_line_discountlinedetail') }} using (_airbyte_line_hashid)
-	left join {{ var('invoices_line_discou__il_discountaccountref') }} using (_airbyte_discountlinedetail_hashid)
-	left join {{ var('invoices_line_discountlinedetail_classref') }} using (_airbyte_discountlinedetail_hashid)
-	left join {{ var('invoices_line_discountlinedetail_taxcoderef') }} using (_airbyte_discountlinedetail_hashid)
+left join {{ var('invoices_line_discou__il_discountaccountref') }} using (_airbyte_discountlinedetail_hashid)
+left join {{ var('invoices_line_discountlinedetail_classref') }} using (_airbyte_discountlinedetail_hashid)
+left join {{ var('invoices_line_discountlinedetail_taxcoderef') }} using (_airbyte_discountlinedetail_hashid)
 left join {{ var('invoices_line_linkedtxn') }} using (_airbyte_line_hashid)
 left join {{ var('invoices_line_salesitemlinedetail') }} using (_airbyte_line_hashid)
-	left join {{ var('invoices_line_salesitemlinedetail_classref') }} using (_airbyte_salesitemlinedetail_hashid)
-	left join {{ var('invoices_line_salesitemlinedetail_itemref') }} using (_airbyte_salesitemlinedetail_hashid)
-		left join {{ var('invoices_line_salesi__detail_itemaccountref') }} using (_airbyte_salesitemlinedetail_hashid)
+left join {{ var('invoices_line_salesitemlinedetail_classref') }} using (_airbyte_salesitemlinedetail_hashid)
+left join {{ var('invoices_line_salesitemlinedetail_itemref') }} using (_airbyte_salesitemlinedetail_hashid)
+left join {{ var('invoices_line_salesi__detail_itemaccountref') }} using (_airbyte_salesitemlinedetail_hashid)
